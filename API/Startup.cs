070b98c7397ed647cd1fc3f00b57af7f652e0353
Maria Eliza <predamariaeliza06@gmail.com>
2021-04-28 15:35:20 +0300

    New Update

--- API/Startup.cs
+++ API/Startup.cs
@@ -43,6 +43,8 @@ namespace API
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
