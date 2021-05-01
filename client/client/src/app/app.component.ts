b6674dde7afc5da9e2bf2b0b96d9fcb58c5683a6
Maria Eliza <predamariaeliza06@gmail.com>
2021-05-01 13:47:39 +0300

    New Update

---
+++ client/client/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+
+  title = '10library';
+  
+  
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+   this.http.get( 'https://localhost:5001/api/products').subscribe((response: any) => 
+   {
+     console.log(response);
+   }, error => {
+     console.log(error);
+   });
+   
+  }
+} 
